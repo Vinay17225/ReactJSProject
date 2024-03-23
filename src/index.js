900f1d0c383c3b47fd90fe12911cc67555ab6d2d
Array_Java <vinay17225@gmail.com>
2024-03-23 23:08:31 +0530

    Added ReactJS project changes

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React  from 'react';
+
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+    <div>
+<h1> Hello World: Vinay Kumar </h1>
+<p> Plz Like and Share</p>
+</div>,
+document.getElementById('root'));
